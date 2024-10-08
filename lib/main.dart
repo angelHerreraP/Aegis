a8afa45dcc96fe37943896b80cedef07244fed40
angelh <a.manuel.herrera.p@gmail.com>
2024-10-08 15:45:55 -0600

    se crean los archivos iniciales splashscreen y login

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mi Aplicación',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Página Principal'),
-        ),
-        body: Center(
-          child: Text('¡Hola, mundo!'),
-        ),
-      ),
+      home: login(),
     );
   }
 }
